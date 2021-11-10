db8844d0ca63d8f89abda5b73dd3977631ee6281
off-dngw <off-dngw@gmail.com>
2021-11-10 10:13:02 +0900

    1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# server
+# serverPractice
+파이널프젝 서버 연습용 레포
---
+++ controller/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+    auth: require('./users/Auth'),
+    register: require('./users/Register'),
+    login: require('./users/LogIn'),
+    logout: require('./users/LogOut'),
+    kakaogooglelogin: require('./users/KakaoGoogleLogIn'),
+    // ( ++++++++++++++++++++ 띄어놓기 띄어놓기 띄어놓기 ++++++++++++++++++++ )
+    healing: require('./healings/Healing'),
+    // ( ++++++++++++++++++++ 띄어놓기 띄어놓기 띄어놓기 ++++++++++++++++++++ )
+    posts: require('./boards/BoardList'),
+    post: require('./boards/Board'),
+    addUpdatePost: require('./boards/BoardAddUpdate'),
+    deletePost: require('./boards/BoardDelete'),
+    comments: require('./boards/CommentList'),
+    addComment: require('./boards/CommentAdd'),
+    deleteComment: require('./boards/CommentDelete'),
+    // ( ++++++++++++++++++++ 띄어놓기 띄어놓기 띄어놓기 ++++++++++++++++++++ )
+    updateUserInfo: require('./myPages/UpdateUserInfo'),
+    unRegister: require('./myPages/UnRegister'),
+    zzim: require('./myPages/ZzimList'),
+    addZzim: require('./myPages/AddZzim'),
+    deleteZzim: require('./myPages/DeleteZzim'),
+    myPosts: require('./myPages/MyPost'),
+};
